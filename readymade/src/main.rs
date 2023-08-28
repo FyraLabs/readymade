b10467440a7cfe3b3a4abbe434a76f22694ad99c
Cappy Ishihara <cappy@cappuchino.xyz>
2023-08-28 23:55:30 +0700

    the actual initial commit :smiley:

--- readymade/src/main.rs
+++ readymade/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+
+// TODO: Do a GUI and CLI for this, maybe.
+
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    // set up tracing for logfmt output
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_level(true)
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+    tracing::debug!("Hello, world!");
+
+    Ok(())
 }
