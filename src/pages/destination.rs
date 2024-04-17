9e41e8795a320a81d024ddc3c9525ed2e729856a
Lleyton Gray <lleyton@fyralabs.com>
2024-04-16 20:16:56 -0700

    style: fixup

--- src/pages/destination.rs
+++ src/pages/destination.rs
@@ -31,53 +31,51 @@ impl SimpleComponent for DestinationPage {
                 set_orientation: gtk::Orientation::Vertical,
                 set_spacing: 4,
                 gtk::Box {
-                set_orientation: gtk::Orientation::Horizontal,
-                set_spacing: 4,
-                set_vexpand: true,
-                set_hexpand: true,
-                set_valign: gtk::Align::Center,
-                set_halign: gtk::Align::Center,
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_spacing: 4,
+                    set_vexpand: true,
+                    set_hexpand: true,
+                    set_valign: gtk::Align::Center,
+                    set_halign: gtk::Align::Center,
 
+                    gtk::Box {
+                        set_orientation: gtk::Orientation::Vertical,
+                        set_spacing: 2,
+
+                        gtk::Image {
+                            set_icon_name: Some("drive-harddisk"),
+                            inline_css: "-gtk-icon-size: 128px"
+                        },
+
+                        gtk::Label {
+                            set_label: "Seagate HDD",
+                            inline_css: "font-size: 16px; font-weight: bold"
+                        },
+
+                        gtk::Label {
+                            set_label: "Ubuntu 20.04"
+                        }
+                    }
+                },
                 gtk::Box {
-                    set_orientation: gtk::Orientation::Vertical,
-                    set_spacing: 2,
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_spacing: 4,
 
-                    gtk::Image {
-                        set_icon_name: Some("drive-harddisk"),
-                        inline_css: "-gtk-icon-size: 128px"
+                    libhelium::TextButton {
+                        set_label: "Previous",
+                        connect_clicked => DestinationPageMsg::Navigate(NavigationAction::Back)
                     },
 
-                    gtk::Label {
-                        set_label: "Seagate HDD",
-                        inline_css: "font-size: 16px; font-weight: bold"
+                    gtk::Box {
+                        set_hexpand: true,
                     },
 
-                    gtk::Label {
-                        set_label: "Ubuntu 20.04"
+                    libhelium::PillButton {
+                        set_label: "Next",
+                        inline_css: "padding-left: 48px; padding-right: 48px",
                     }
                 }
-            },
-
-            gtk::Box {
-                set_orientation: gtk::Orientation::Horizontal,
-                set_spacing: 4,
-
-                libhelium::TextButton {
-                    set_label: "Previous",
-                    connect_clicked => DestinationPageMsg::Navigate(NavigationAction::Back)
-                },
-
-                gtk::Box {
-                    set_hexpand: true,
-                },
-
-                libhelium::PillButton {
-                    set_label: "Next",
-                                        inline_css: "padding-left: 48px; padding-right: 48px",
-                }
             }
-            }
-            // insert logo here i guess, branding time
         }
     }
 
