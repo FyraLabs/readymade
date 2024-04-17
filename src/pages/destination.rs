7b446b4c53cfbc27b8b88bdad09e84af9bdfc550
Lleyton Gray <lleyton@fyralabs.com>
2024-04-17 02:27:00 -0700

    feat: disk list from Vec

--- src/pages/destination.rs
+++ src/pages/destination.rs
@@ -1,14 +1,65 @@
 use crate::NavigationAction;
 use gtk::prelude::*;
 use libhelium::prelude::*;
-use relm4::{ComponentParts, ComponentSender, RelmWidgetExt, SimpleComponent};
+use relm4::{ComponentParts, ComponentSender, RelmWidgetExt, SimpleComponent, WidgetTemplate, factory::{FactoryComponent, FactorySender, DynamicIndex, FactoryVecDeque}};
 
-pub struct DestinationPage {}
+struct DiskInit {
+    disk_name: String,
+    os_name: String
+}
+
+struct Disk {
+    disk_name: String,
+    os_name: String
+}
+
+#[relm4::factory]
+impl FactoryComponent for Disk {
+    type Init = DiskInit;
+    type Input = ();
+    type Output = ();
+    type CommandOutput = ();
+    type ParentWidget = gtk::FlowBox;
+
+    view! {
+        #[root]
+        gtk::Box {
+            set_orientation: gtk::Orientation::Vertical,
+            set_spacing: 2,
+
+            gtk::Image {
+                set_icon_name: Some("drive-harddisk"),
+                inline_css: "-gtk-icon-size: 128px"
+            },
+
+            gtk::Label {
+                set_label: &self.disk_name,
+                inline_css: "font-size: 16px; font-weight: bold"
+            },
+
+            gtk::Label {
+                set_label: &self.os_name
+            }
+        }
+    }
+
+    fn init_model(value: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
+        Self {
+            disk_name: value.disk_name,
+            os_name: value.os_name
+        }
+    }
+}
+
+pub struct DestinationPage {
+    disks: FactoryVecDeque<Disk>
+}
 
 #[derive(Debug)]
 pub enum DestinationPageMsg {
     #[doc(hidden)]
     Navigate(NavigationAction),
+    SelectionChanged
 }
 
 #[derive(Debug)]
@@ -30,32 +81,15 @@ impl SimpleComponent for DestinationPage {
             add = &gtk::Box {
                 set_orientation: gtk::Orientation::Vertical,
                 set_spacing: 4,
-                gtk::Box {
+                #[local_ref]
+                disk_list -> gtk::FlowBox {
                     set_orientation: gtk::Orientation::Horizontal,
-                    set_spacing: 4,
                     set_vexpand: true,
                     set_hexpand: true,
                     set_valign: gtk::Align::Center,
                     set_halign: gtk::Align::Center,
-
-                    gtk::Box {
-                        set_orientation: gtk::Orientation::Vertical,
-                        set_spacing: 2,
-
-                        gtk::Image {
-                            set_icon_name: Some("drive-harddisk"),
-                            inline_css: "-gtk-icon-size: 128px"
-                        },
-
-                        gtk::Label {
-                            set_label: "Seagate HDD",
-                            inline_css: "font-size: 16px; font-weight: bold"
-                        },
-
-                        gtk::Label {
-                            set_label: "Ubuntu 20.04"
-                        }
-                    }
+                    set_min_children_per_line: 7,
+                    connect_selected_children_changed => DestinationPageMsg::SelectionChanged
                 },
                 gtk::Box {
                     set_orientation: gtk::Orientation::Horizontal,
@@ -84,7 +118,31 @@ impl SimpleComponent for DestinationPage {
         root: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = Self {};
+        let mut disks = FactoryVecDeque::builder()
+            .launch(gtk::FlowBox::default())
+            .forward(sender.input_sender(), |output| match output {
+                _ => todo!()
+            });
+
+        disks.guard().push_front(DiskInit {
+            disk_name: "fuck".to_string(),
+            os_name: "owo".to_string()
+        });
+
+        disks.guard().push_front(DiskInit {
+            disk_name: "fuck".to_string(),
+            os_name: "owo".to_string()
+        });
+
+                disks.guard().push_front(DiskInit {
+            disk_name: "fuck".to_string(),
+            os_name: "owo".to_string()
+        });
+
+
+        let model = Self {disks};
+
+        let disk_list = model.disks.widget();
         let widgets = view_output!();
 
         ComponentParts { model, widgets }
@@ -95,6 +153,7 @@ impl SimpleComponent for DestinationPage {
             DestinationPageMsg::Navigate(action) => sender
                 .output(DestinationPageOutput::Navigate(action))
                 .unwrap(),
+            DestinationPageMsg::SelectionChanged => {}
         }
     }
 }
