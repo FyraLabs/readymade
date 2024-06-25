166597310db1847fcb471d2ac049a3a8bc59425e
madonuko <madonuko@outlook.com>
2024-06-25 18:43:17 +0800

    feat: wip on repartcfg structs

--- src/backend/repartcfg.rs
+++ src/backend/repartcfg.rs
@@ -1,4 +1,31 @@
-use std::env::consts::ARCH;
+use bytesize::ByteSize;
+use serde_with::{
+    formats::{ColonSeparator, SemicolonSeparator, SpaceSeparator},
+    serde_as, StringWithSeparator,
+};
+use std::{env::consts::ARCH, path::PathBuf};
+
+use crate::ini_enum;
+
+#[derive(Debug, Copy, Clone, Default)]
+pub struct Size {
+    pub inner: ByteSize,
+}
+
+impl From<ByteSize> for Size {
+    fn from(value: ByteSize) -> Self {
+        Self { inner: value }
+    }
+}
+
+impl serde::Serialize for Size {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_u64(self.inner.as_u64())
+    }
+}
 
 //* https://www.freedesktop.org/software/systemd/man/latest/repart.d.html
 
@@ -8,14 +35,90 @@ pub struct RepartConfig {
     partition: Partition,
 }
 
-#[derive(serde::Serialize)]
+#[serde_as]
+#[derive(serde::Serialize, validator::Validate)]
 #[serde(rename_all = "PascalCase")]
 pub struct Partition {
-    r#type: PartTypeIdent
+    r#type: PartTypeIdent,
+    label: String,
+    #[serde(rename = "UUID")]
+    uuid: uuid::Uuid,
+    priority: i32,
+    #[validate(range(min = 0, max = 1_000_000))]
+    #[serde(default = "_default_weight")]
+    weight: u32,
+    #[validate(range(min = 0, max = 1_000_000))]
+    #[serde(default)]
+    padding_weight: u32,
+    #[serde(default = "_default_size_min_bytes")]
+    size_min_bytes: Size,
+    #[serde(default)]
+    size_max_bytes: Size,
+    #[serde(default)]
+    padding_min_bytes: Size,
+    #[serde(default)]
+    padding_max_bytes: Size,
+    copy_blocks: Option<either::Either<PathBuf, String>>,
+    format: Option<FileSystem>,
+    #[serde(default)]
+    #[serde_as(as = "StringWithSeparator::<ColonSeparator, String>")]
+    copy_files: Vec<String>,
+
+    // todo: serialize ; and whitespace-separated values as vec
+
+    // separate by ;
+    #[serde(default)]
+    #[serde_as(as = "StringWithSeparator::<SemicolonSeparator, String>")]
+    exclude_files: Vec<String>,
+    #[serde(default)]
+    #[serde_as(as = "StringWithSeparator::<SemicolonSeparator, String>")]
+    exclude_files_target: Vec<String>,
+
+    // separate by whitespace
+    #[serde(default)]
+    #[serde_as(as = "StringWithSeparator::<SpaceSeparator, String>")]
+    make_directories: Vec<String>,
+
+    #[serde(default)]
+    #[serde_as(as = "StringWithSeparator::<SpaceSeparator, String>")]
+    subvolumes: Vec<String>,
+    default_subvolume: Option<String>,
+    #[serde(default)]
+    encrypt: either::Either<bool, EncryptOption>,
+    #[serde(default)]
+    verity: Verity,
+
+    #[serde(default)]
+    #[serde(serialize_with = "turn_to_string")]
+    factory_reset: bool,
+
+    // Takes at least one and at most two fields separated with a colon (":").
+    // #[serde_as(as = "(PathBuf, StringWithSeparator::<SpaceSeparator, String>)")]
+    // #[serde_as(as = "StringWithSeparator::<ColonSeparator, (PathBuf, StringWithSeparator::<CommaSeperator, String)>")]
+    // mount_point: (PathBuf, Option<String>),
+    #[serde(default)]
+    mount_point: String,
 }
 
-#[derive(serde_enum_str::Deserialize_enum_str, serde_enum_str::Serialize_enum_str, Default)]
-#[serde(rename_all = "kebab-case")]
+fn turn_to_string<T, S>(value: &T, se: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+    T: std::fmt::Display,
+{
+    se.serialize_str(&format!("{value}"))
+}
+
+const fn _default_weight() -> u32 {
+    1000
+}
+
+const fn _default_size_min_bytes() -> Size {
+    Size {
+        inner: ByteSize::mib(10),
+    }
+}
+
+#[derive(Debug, Default)]
 pub enum PartTypeIdent {
     Esp,
     Xbootldr,
@@ -32,11 +135,8 @@ pub enum PartTypeIdent {
     RootSecondary,
     RootSecondaryVerity,
     RootSecondaryVeritySig,
-    // #[serde(rename = concat!("root-", ARCH))]
     RootArch,
-    // #[serde(rename = concat!("root-", ARCH, "-verity"))]
     RootArchVerity,
-    // #[serde(rename = concat!("root-", ARCH, "-verity-sig"))]
     RootArchVeritySig,
     Usr,
     UsrVerity,
@@ -44,27 +144,124 @@ pub enum PartTypeIdent {
     UsrSecondary,
     UsrSecondaryVerity,
     UsrSecondaryVeritySig,
-    // #[serde(rename = concat!("root-", ARCH))]
     UsrArch,
-    // #[serde(rename = concat!("root-", ARCH, "-verity"))]
     UsrArchVerity,
-    // #[serde(rename = concat!("root-", ARCH, "-verity-sig"))]
     UsrArchVeritySig,
-    #[serde(other)]
-    Others(String)
+
+    Unknown(String),
 }
 
-const ROOTARCH = concat!("root-", ARCH);
-const
+const ROOT_ARCH: &str = const_format::formatcp!("root-{ARCH}");
+const ROOT_ARCH_VERITY: &str = const_format::formatcp!("root-{ARCH}-verity");
+const ROOT_ARCH_VERITY_SIG: &str = const_format::formatcp!("root-{ARCH}-verity-sig");
+const USR_ARCH: &str = const_format::formatcp!("usr-{ARCH}");
+const USR_ARCH_VERITY: &str = const_format::formatcp!("usr-{ARCH}-verity");
+const USR_ARCH_VERITY_SIG: &str = const_format::formatcp!("usr-{ARCH}-verity-sig");
 
-impl std::borrow::Borrow<str> for PartTypeIdent {
-    fn borrow(&self) -> &str {
-        if let Self::Others(s) = self {
-            match s {
-                ROOTARCH => {
+impl serde::Serialize for PartTypeIdent {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(match self {
+            Self::Esp => "esp",
+            Self::Xbootldr => "xbootldr",
+            Self::Swap => "swap",
+            Self::Home => "home",
+            Self::Srv => "srv",
+            Self::Var => "var",
+            Self::Tmp => "tmp",
+            Self::LinuxGeneric => "linux-generic",
+            Self::Root => "root",
+            Self::RootVerity => "root-verity",
+            Self::RootVeritySig => "root-verity-sig",
+            Self::RootSecondary => "root-secondary",
+            Self::RootSecondaryVerity => "root-secondary-verity",
+            Self::RootSecondaryVeritySig => "root-secondary-verity-sig",
+            Self::RootArch => ROOT_ARCH,
+            Self::RootArchVerity => ROOT_ARCH_VERITY,
+            Self::RootArchVeritySig => ROOT_ARCH_VERITY_SIG,
+            Self::Usr => "usr",
+            Self::UsrVerity => "usr-verity",
+            Self::UsrVeritySig => "usr-verity-sig",
+            Self::UsrSecondary => "usr-secondary",
+            Self::UsrSecondaryVerity => "usr-secondary-verity",
+            Self::UsrSecondaryVeritySig => "usr-secondary-verity-sig",
+            Self::UsrArch => USR_ARCH,
+            Self::UsrArchVerity => USR_ARCH_VERITY,
+            Self::UsrArchVeritySig => USR_ARCH_VERITY_SIG,
+            Self::Unknown(x) => x,
+        })
+    }
+}
 
-                }
-            }
-        }
+ini_enum! {
+    #[derive(Debug)]
+    pub enum FileSystem {
+        Ext4,
+        Btrfs,
+        Xfs,
+        Vfat,
+        Erofs,
+        Squashfs,
+        Swap,
+    }
+
+    #[derive(Debug)]
+    pub enum EncryptOption {
+        Off,
+        KeyFile,
+        Tpm2,
+        KeyFileTpm2 => "key-file+tpm2",
+    }
+
+    #[derive(Debug, Default)]
+    pub enum Verity {
+        #[default]
+        Off,
+        Data,
+        Hash,
+        Signature,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::{Partition, RepartConfig};
+
+    #[test]
+    fn ser_new_config() {
+        let res = serde_ini::to_string(&RepartConfig {
+            partition: Partition {
+                r#type: super::PartTypeIdent::Esp,
+                label: "My Label".to_string(),
+                uuid: uuid::uuid!("7466c448-87ac-4e1c-b3e3-fe83b7a19262"),
+                priority: Default::default(),
+                weight: Default::default(),
+                padding_weight: Default::default(),
+                size_min_bytes: super::_default_size_min_bytes(),
+                size_max_bytes: super::Size {
+                    inner: bytesize::ByteSize::kb(100),
+                },
+                padding_min_bytes: super::Size::default(),
+                padding_max_bytes: super::Size::default(),
+                copy_blocks: Some(either::Left(PathBuf::from("hai"))),
+                format: Some(super::FileSystem::Ext4),
+                copy_files: vec![],
+                exclude_files: vec![],
+                exclude_files_target: vec![],
+                make_directories: vec![],
+                subvolumes: vec![],
+                default_subvolume: None,
+                encrypt: either::Either::Left(false),
+                verity: super::Verity::Off,
+                factory_reset: false,
+                mount_point: "idk".to_string(),
+            },
+        })
+        .unwrap();
+        println!("{res}");
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -71,3 +71,89 @@ pub(crate) fn array_str_to_values<const N: usize>(arr: [&str; N]) -> Vec<serde_j
 pub fn is_chromebook() -> bool {
     std::fs::metadata("/dev/cros_ec").is_ok()
 }
+
+/// Make an enum and impl Serialize
+///
+/// # Examples
+/// ```rs
+/// ini_enum! {
+///     pub enum Idk {
+///         A,
+///         B,
+///         C,
+///     }
+/// }
+/// ```
+#[macro_export]
+macro_rules! ini_enum {
+    (@match $field:ident) => {{
+        stringify!(paste::paste! { [<$field:snake>] }).replace('_', "-")
+    }};
+    (@match $field:ident => $s:literal) => {{
+        $s.to_string()
+    }};
+    (
+        $(#[$outmeta:meta])*
+        $v:vis enum $name:ident {
+            $(
+                $(#[$meta:meta])?
+                $field:ident $(=> $s:literal)?,
+            )*$(,)?
+        }
+    ) => {
+        $(#[$outmeta])*
+        $v enum $name {$(
+            $(#[$meta])?
+            $field,
+        )*}
+        impl serde::Serialize for $name {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: ::serde::Serializer,
+            {
+                serializer.serialize_str(&match self {$(
+                    Self::$field => ini_enum! { @match $field $(=> $s)? },
+                )*})
+            }
+        }
+    };
+    (
+        $(#[$outmeta1:meta])*
+        $v1:vis enum $name1:ident {
+            $(
+                $(#[$meta1:meta])?
+                $field1:ident $(=> $s1:literal)?,
+            )*$(,)?
+        }
+        $(
+            $(#[$outmeta:meta])*
+            $v:vis enum $name:ident {
+                $(
+                    $(#[$meta:meta])?
+                    $field:ident $(=> $s:literal)?,
+                )*$(,)?
+            }
+        )+
+    ) => {
+        ini_enum! {
+            $(
+                $(#[$outmeta])*
+                $v enum $name {
+                    $(
+                        $(#[$meta])?
+                        $field $(=> $s)?,
+                    )*
+                }
+            )+
+        }
+        ini_enum! {
+            $(#[$outmeta1])*
+            $v1 enum $name1 {
+                $(
+                    $(#[$meta1])?
+                    $field1 $(=> $s1)?,
+                )*
+            }
+        }
+    }
+}
