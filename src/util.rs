a844b747834c8adbb36131cb2321fb86f35cb746
madonuko <madonuko@outlook.com>
2024-07-26 15:07:36 +0800

    feat: impl `copy_dir()`

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,6 @@
 //! QoL Utilities for Readymade
 
+use std::path::Path;
 pub const LIVE_BASE: &str = "/dev/mapper/live-base";
 pub const ROOTFS_BASE: &str = "/run/rootfsbase";
 
@@ -188,7 +189,7 @@ pub fn exist_then<T: Default>(r: std::io::Result<T>) -> std::io::Result<T> {
 }
 
 /// Ignore errors about nonexisting files.
-pub fn exist_then_read_dir<A: AsRef<std::path::Path>>(
+pub fn exist_then_read_dir<A: AsRef<Path>>(
     p: A,
 ) -> std::io::Result<Box<dyn Iterator<Item = std::fs::DirEntry>>> {
     match std::fs::read_dir(p) {
@@ -196,4 +197,24 @@ pub fn exist_then_read_dir<A: AsRef<std::path::Path>>(
         Err(e) => Err(e),
         Ok(x) => Ok(Box::new(x.flatten())),
     }
+}
+
+pub fn copy_dir<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> Result<(), std::io::Error> {
+    use rayon::iter::{ParallelBridge, ParallelIterator};
+
+    let to = to.as_ref();
+    std::fs::create_dir_all(to)?;
+    from.as_ref()
+        .read_dir()?
+        .par_bridge()
+        .try_for_each(|dir_entry| -> std::io::Result<()> {
+            let dir_entry = dir_entry?;
+            let to = to.join(dir_entry.file_name());
+            if dir_entry.file_type()?.is_dir() {
+                copy_dir(dir_entry.path(), to)?;
+            } else {
+                std::fs::copy(dir_entry.path(), to)?;
+            }
+            Ok(())
+        })
 }
