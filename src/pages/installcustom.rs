d68ad44c68e8c5387cff31c91bd4428b88bc5cb0
madonuko <madonuko@outlook.com>
2024-10-02 16:54:08 +0800

    feat(pages/installcustom): blanket impl for ChooseMount gui factory

--- src/pages/installcustom.rs
+++ src/pages/installcustom.rs
@@ -1,3 +1,7 @@
+use std::path::PathBuf;
+
+use relm4::factory::{positions::GridPosition, Position};
+
 use crate::{prelude::*, NavigationAction};
 
 pub struct InstallCustomPage;
@@ -46,5 +50,65 @@ impl SimpleComponent for InstallCustomPage {
                 .output(InstallCustomPageOutput::Navigate(action))
                 .unwrap(),
         }
+    }
+}
+
+// ────────────────────────────────────────────────────────────────────────────
+
+#[derive(Debug, Clone, Default)]
+struct ChooseMount {
+    partition: PathBuf,
+    mountpoint: PathBuf,
+    options: String,
+}
+
+#[derive(Debug)]
+enum ChooseMountMsg {
+    Remove,
+}
+
+#[relm4::factory]
+impl FactoryComponent for ChooseMount {
+    type ParentWidget = gtk::Box;
+    type Input = ChooseMountMsg;
+    type Output = ();
+    type CommandOutput = ();
+    type Init = ();
+
+    view! {
+        gtk::Box {
+            set_orientation: gtk::Orientation::Horizontal,
+
+            // FIXME: make this a dropdown?
+            libhelium::TextField {
+                set_is_outline: true,
+                set_margin_top: 6,
+                set_margin_bottom: 6,
+                set_placeholder_text: &gettext("Partition"),
+            },
+
+            libhelium::TextField {
+                set_is_outline: true,
+                set_margin_top: 6,
+                set_margin_bottom: 6,
+                set_placeholder_text: &gettext("Mountpoint"),
+            },
+
+            libhelium::Button {
+                set_icon_name: "gtk-edit",
+                set_tooltip: &gettext("Mount options"),
+                // TODO: connect_clicked
+            }
+
+            libhelium::Button {
+                set_icon_name: "delete",
+                set_tooltip: &gettext("Remove mountpoint"),
+                // TODO: connect_clicked
+            }
+        }
+    }
+
+    fn init_model(_init: Self::Init, _index: &Self::Index, _sender: FactorySender<Self>) -> Self {
+        Self::default()
     }
 }
