a25a03551db4bcd25b2c6e8bd656c2ce86ce9bc0
Cappy Ishihara <cappy@cappuchino.xyz>
2024-12-27 12:06:16 +0700

    lazy umount

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,7 +2,7 @@
 
 # Cleanup artifacts from setup-dev and run-dev
 
-sudo umount dev/install_root -R
+sudo umount dev/install_root -Rl
 sudo rm -rf dev/test.img
 sudo losetup -D
 fallocate -l 8G dev/test.img
