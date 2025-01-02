3c9a48083369f3bb190ca47edd820dc0c6fcdce7
Lleyton Gray <lleyton@fyralabs.com>
2025-01-01 17:19:32 -0800

    feat: add stages for long running postinstall sections

--- src/backend/postinstall/selinux.rs
+++ src/backend/postinstall/selinux.rs
@@ -2,6 +2,8 @@ use color_eyre::{eyre::bail, Result};
 use serde::{Deserialize, Serialize};
 use std::process::Command;
 
+use crate::stage;
+
 use super::{Context, PostInstallModule};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
@@ -9,18 +11,20 @@ pub struct SELinux;
 
 impl PostInstallModule for SELinux {
     fn run(&self, _context: &Context) -> Result<()> {
-        let setfiles_cmd_status = Command::new("setfiles")
-            .args(["-e", "/proc", "-e", "/sys"])
-            .arg("/etc/selinux/targeted/contexts/files/file_contexts")
-            .arg("/")
-            .status()?;
+        stage!("Setting SELinux labels" {
+            let setfiles_cmd_status = Command::new("setfiles")
+                .args(["-e", "/proc", "-e", "/sys"])
+                .arg("/etc/selinux/targeted/contexts/files/file_contexts")
+                .arg("/")
+                .status()?;
 
-        if !setfiles_cmd_status.success() {
-            bail!(
-                "dracut failed with exit code {:?}",
-                setfiles_cmd_status.code()
-            );
-        }
+            if !setfiles_cmd_status.success() {
+                bail!(
+                    "dracut failed with exit code {:?}",
+                    setfiles_cmd_status.code()
+                );
+            }
+        });
 
         Ok(())
     }
