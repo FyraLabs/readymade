efcbc4999b487bfa4c84a43dde6d704d4c60e5ac
Cappy Ishihara <cappy@cappuchino.xyz>
2025-01-25 00:53:48 +0700

    rustfmt

--- src/backend/postinstall/efi_stub.rs
+++ src/backend/postinstall/efi_stub.rs
@@ -54,11 +54,11 @@ impl PostInstallModule for EfiStub {
         if !status.success() {
             // We should be able to fail silently here, as the user may still be able to find the OS as long
             // as the firmware finds the bootloader binary
-            
+
             tracing::error!("Failed to create EFI boot entry");
-            
+
             tracing::warn!("EFI boot entry creation failed, You may not be able to find the installed OS in the boot menu");
-            
+
             // todo: Implement a popup to warn the user, since this is not a critical error but should be noted
         }
 
