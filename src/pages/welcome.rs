acd80f69feee79706962ac468e94800acdc69798
madonuko <madonuko@outlook.com>
2025-02-02 02:27:08 +0800

    refactor(pages/welcome): kurage

--- src/pages/welcome.rs
+++ src/pages/welcome.rs
@@ -1,110 +1,63 @@
 use crate::prelude::*;
-use crate::NavigationAction;
-use crate::INSTALLATION_STATE;
-use relm4::{ComponentParts, ComponentSender, RelmWidgetExt, SimpleComponent};
 
-pub struct WelcomePage;
+page!(Welcome:
+    init(root, sender, model, widgets) {
+        root.set_title(None::<&gtk::Window>); // unset the title from page!()
+    }
 
-#[derive(Debug)]
-pub enum WelcomePageMsg {
-    #[doc(hidden)]
-    Navigate(NavigationAction),
-    Update,
-}
+    update(self, message, sender) {} => {}
 
-#[derive(Debug)]
-pub enum WelcomePageOutput {
-    Navigate(NavigationAction),
-}
 
-#[relm4::component(pub)]
-impl SimpleComponent for WelcomePage {
-    type Init = ();
-    type Input = WelcomePageMsg;
-    type Output = WelcomePageOutput;
+    gtk::Box {
+        set_orientation: gtk::Orientation::Vertical,
+        set_spacing: 16,
+        set_vexpand: true,
+        set_valign: gtk::Align::Center,
+        set_halign: gtk::Align::Center,
 
-    view! {
-        libhelium::ViewMono {
-        append = &gtk::Box {
-            set_orientation: gtk::Orientation::Vertical,
-            set_spacing: 4,
-            set_margin_all: 16,
+        gtk::Image {
+            set_icon_name: Some(&crate::CONFIG.read().distro.icon),
+            inline_css: "-gtk-icon-size: 128px",
+        },
+
+        gtk::Label {
+            #[watch]
+            set_label: &gettext("Welcome to %s").replace("%s", &crate::CONFIG.read().distro.name),
+            inline_css: "font-weight: bold; font-size: 1.75rem",
+        },
+
+        gtk::Label {
+            #[watch]
+            set_label: &gettext(r#"Either test %s from this installer or start the installation now. You can always return to this screen by selecting "Installer" in the menu."#).replace("%s", &crate::CONFIG.read().distro.name),
+            set_justify: gtk::Justification::Center,
+            set_max_width_chars: 60,
+            set_wrap: true
+        },
+    },
+
+    gtk::Box {
+        set_spacing: 4,
+        // set_halign: gtk::Align::Center,
+
+        libhelium::Button {
+            set_is_pill: true,
+            #[watch]
+            set_label: &gettext("Try"),
+            add_css_class: "large-button",
+            connect_clicked => WelcomePageMsg::Navigate(NavigationAction::Quit)
+        },
 
             gtk::Box {
-                set_orientation: gtk::Orientation::Vertical,
-                set_spacing: 16,
-                set_vexpand: true,
-                set_valign: gtk::Align::Center,
-                set_halign: gtk::Align::Center,
-
-                gtk::Image {
-                    set_icon_name: Some(&crate::CONFIG.read().distro.icon),
-                    inline_css: "-gtk-icon-size: 128px",
-                },
-
-                gtk::Label {
-                    #[watch]
-                    set_label: &gettext("Welcome to %s").replace("%s", &crate::CONFIG.read().distro.name),
-                    inline_css: "font-weight: bold; font-size: 1.75rem",
-                },
-
-                gtk::Label {
-                    #[watch]
-                    set_label: &gettext(r#"Either test %s from this installer or start the installation now. You can always return to this screen by selecting "Installer" in the menu."#).replace("%s", &crate::CONFIG.read().distro.name),
-                    set_justify: gtk::Justification::Center,
-                    set_max_width_chars: 60,
-                    set_wrap: true
-                },
+                set_hexpand: true,
             },
 
-            gtk::Box {
-                set_spacing: 4,
-                // set_halign: gtk::Align::Center,
-
-                libhelium::Button {
-                    set_is_pill: true,
-                    #[watch]
-                    set_label: &gettext("Try"),
-                    add_css_class: "large-button",
-                    connect_clicked => WelcomePageMsg::Navigate(NavigationAction::Quit)
-                },
-
-                    gtk::Box {
-                        set_hexpand: true,
-                    },
-
-                libhelium::Button {
-                    set_is_pill: true,
-                    #[watch]
-                    set_label: &gettext("Install"),
-                    add_css_class: "suggested-action",
-                    add_css_class: "large-button",
-                    connect_clicked => WelcomePageMsg::Navigate(NavigationAction::GoTo(crate::Page::Destination))
-                }
-            }
+        libhelium::Button {
+            set_is_pill: true,
+            #[watch]
+            set_label: &gettext("Install"),
+            add_css_class: "suggested-action",
+            add_css_class: "large-button",
+            connect_clicked => WelcomePageMsg::Navigate(NavigationAction::GoTo(crate::Page::Destination))
         }
     }
-    }
-
-    fn init(
-        _init: Self::Init,
-        root: Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let model = Self {};
-        let widgets = view_output!();
-
-        INSTALLATION_STATE.subscribe(sender.input_sender(), |_| WelcomePageMsg::Update);
-
-        ComponentParts { model, widgets }
-    }
-
-    fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
-        match message {
-            WelcomePageMsg::Navigate(action) => {
-                sender.output(WelcomePageOutput::Navigate(action)).unwrap();
-            }
-            WelcomePageMsg::Update => {}
-        }
-    }
-}
+);
