979feff389434416e23e1ec828276aa4f79839b3
madonuko <madonuko@outlook.com>
2025-04-26 12:34:30 +0800

    Revert "chore: try to use newer chumsky"
    This reverts commit 68916b9443f651241218b013a5d9047f0b093145.

--- serde-systemd-unit/src/parser.rs
+++ serde-systemd-unit/src/parser.rs
@@ -2,14 +2,11 @@ use chumsky::prelude::*;
 use std::collections::HashMap;
 
 pub(crate) type IniIntermediate = HashMap<String, Vec<(String, String)>>;
-pub(crate) type Err<'s> = Vec<chumsky::error::Rich<'s, char>>;
-pub(crate) type Ext<'s> = chumsky::extra::Err<Rich<'s, char>>;
+pub(crate) type Err = Vec<chumsky::error::Simple<char>>;
 
 #[must_use]
-fn parser<'s>() -> impl Parser<'s, &'s str, IniIntermediate, Ext<'s>> {
-    let inline_space = any::<_, Ext>()
-        .filter(|c: &char| c.is_whitespace() && *c != '\n')
-        .repeated();
+fn parser() -> impl Parser<char, IniIntermediate, Error = Simple<char>> {
+    let inline_space = filter(|c: &char| c.is_whitespace() && *c != '\n').repeated();
     let empty_lines = just('\n').then(inline_space).repeated().ignored();
     let root_comments = (just('#').or(just(';')))
         .then(none_of('\n').repeated())
@@ -20,16 +17,17 @@ fn parser<'s>() -> impl Parser<'s, &'s str, IniIntermediate, Ext<'s>> {
         .allow_trailing()
         .ignored();
     let uni = |radix: u32, len: usize| {
-        let p = any().filter(move |c: &char| c.is_digit(radix));
-        p.repeated()
-            .exactly(len)
-            .collect()
-            .validate(move |digits: String, e, emitter| {
+        let p = filter(move |c: &char| c.is_digit(radix));
+        p.repeated().exactly(len).collect().validate(
+            move |digits: String,
+                  span: <chumsky::error::Simple<char> as chumsky::Error<_>>::Span,
+                  emit| {
                 char::from_u32(u32::from_str_radix(&digits, radix).unwrap()).unwrap_or_else(|| {
-                    emitter.emit(Rich::custom(e.span(), "invalid unicode character"));
+                    emit(Simple::custom(span, "invalid unicode character"));
                     '\u{FFFD}' // unicode replacement character
                 })
-            })
+            },
+        )
     };
 
     let esc = just('\\')
@@ -53,11 +51,10 @@ fn parser<'s>() -> impl Parser<'s, &'s str, IniIntermediate, Ext<'s>> {
         )))
         .boxed();
     let genqt = |q: char| {
-        (none_of([q, '\\'])
-            .or(esc.clone().filter(|x| x.is_some()).map(|x| x.unwrap()))
-            .repeated())
-        .collect::<String>()
-        .delimited_by(just(q), just(format!("{q}\n")))
+        (none_of([q, '\\']).map(Some).or(esc.clone()).repeated())
+            .flatten()
+            .collect()
+            .delimited_by(just(q), just(format!("{q}\n")))
     };
 
     let s = (genqt('\'').or(genqt('\"')).then_ignore(inline_space)).boxed();
@@ -69,19 +66,18 @@ fn parser<'s>() -> impl Parser<'s, &'s str, IniIntermediate, Ext<'s>> {
             text::ident()
                 .then_ignore(just('=').padded())
                 .then(
-                    s.or(any::<_, Ext>().and_is(just("\\\n").or(just("\n")).not()).map(|x:char|x).repeated() // any chars except [\\]\n
-                        .collect::<String>())
+                    s.or((just("\\\n").or(just("\n")).not().repeated()) // any chars except [\\]\n
                         .separated_by(
                             (root_comments.then(just('\n')).or_not())
-                                .delimited_by(just("\\\n"), inline_space).ignored(),
+                                .delimited_by(just("\\\n"), inline_space),
                         )
-                            .collect::<Vec<String>>()
-                        .map(|v| v.join(""))
+                        .flatten()
+                        .collect::<String>()),
                 )
                 .separated_by(newline_or_comment.clone())
                 .allow_leading()
                 .allow_trailing()
-                .collect::<Vec<(String, String)>>(),
+                .collect::<Vec<(_, _)>>(),
         )
         .separated_by(newline_or_comment.clone())
         .allow_leading()
@@ -95,7 +91,7 @@ fn parser<'s>() -> impl Parser<'s, &'s str, IniIntermediate, Ext<'s>> {
 ///
 /// Returns a list of errors if the input is not a valid Systemd unit format
 pub fn parse_str(s: &str) -> Result<IniIntermediate, Err> {
-    parser().parse(s).into_result()
+    parser().parse(s)
 }
 
 #[cfg(test)]
