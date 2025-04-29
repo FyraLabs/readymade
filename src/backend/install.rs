FyraLabs/readymade

commit 62d0a2e8ba661686a90eaf3fe827aa712141ce85

refactor: part 4 random tidying up here and there

src/backend/install.rs
@@ -459,27 +459,19 @@ impl FinalInstallationState {
 
     // This cleans up any folder that is not on the bootc whitelist from a bootc-installed filesystem
     fn bootc_cleanup(mountpoint: &Path) -> Result<()> {
-        std::fs::read_dir(mountpoint)?.for_each(|f| {
-            let Ok(file) = f else {
-                return;
-            };
-            let Ok(file_name) = file.file_name().into_string() else {
-                return;
-            };
-            let Ok(file_type) = file.file_type() else {
-                return;
-            };
-
-            match file_name.as_str() {
-                "boot" | "ostree" | "efi" | ".bootc-aleph.json" => {}
+        _ = std::fs::read_dir(mountpoint)?.try_for_each(|f| {
+            let f = f?;
+            match f.file_name().as_encoded_bytes() {
+                b"boot" | b"ostree" | b"efi" | b".bootc-aleph.json" => {}
                 _ => {
-                    if file_type.is_dir() {
-                        std::fs::remove_dir_all(file.path()).ok();
+                    _ = if f.file_type()?.is_dir() {
+                        std::fs::remove_dir_all(f.path())
                     } else {
-                        std::fs::remove_file(file.path()).ok();
+                        std::fs::remove_file(f.path())
                     }
                 }
             }
+            std::io::Result::Ok(())
         });
         Ok(())
     }
@@ -569,11 +561,7 @@ impl FinalInstallationState {
             )
             .args(["--karg=rhgb", "--karg=quiet", "--karg=splash"])
             .arg(target_root)
-            .args(
-                bootc_target_imgref
-                    .iter()
-                    .flat_map(|a| ["--target-imgref", a]),
-            )
+            .args((bootc_target_imgref.iter()).flat_map(|a| ["--target-imgref", a]))
             .args(bootc_kargs.iter().flat_map(|e| ["--karg", e]))
             .args(bootc_enforce_sigpolicy.then_some("--enforce-container-sigpolicy"))
             .status()
@@ -645,9 +633,8 @@ impl FinalInstallationState {
 
         // Write the state dump to the chroot
         let state_dump_path = Path::new(crate::consts::READYMADE_STATE_PATH);
-        let parent = state_dump_path
-            .parent()
-            .ok_or_else(|| eyre!("Invalid state dump path - no parent directory"))?;
+        let parent =
+            (state_dump_path.parent()).context("Invalid state dump path - no parent directory")?;
         std::fs::create_dir_all(parent)
             .wrap_err("Failed to create parent directories for state dump")?;
         std::fs::write(
@@ -759,8 +746,7 @@ impl FinalInstallationState {
             .unwrap_or(false)
         {
             tracing::info!(
-                "Using {} as copy source, as it exists presumably due to raw rootfs in dracut",
-                ROOTFS_BASE
+                "Using {ROOTFS_BASE} as copy source, as it exists presumably due to raw rootfs in dracut"
             );
             ROOTFS_BASE.to_owned()
         }
@@ -833,45 +819,25 @@ impl FinalInstallationState {
         is_bootc: bool,
     ) -> Result<crate::backend::repart_output::RepartOutput> {
         let copy_source = Self::determine_copy_source();
-
         let dry_run =
             std::env::var("READYMADE_DRY_RUN").map_or(cfg!(debug_assertions), |v| v == "1");
-        let dry_run = if dry_run { "yes" } else { "no" };
-
-        let mut args = vec![
-            "--dry-run",
-            dry_run,
-            "--definitions",
-            cfgdir.to_str().unwrap(),
-            "--empty",
-            "force",
-            "--offline",
-            "false",
-            "--json",
-            "pretty",
-        ];
-
-        if !is_bootc {
-            args.extend_from_slice(&["--copy-source", &copy_source]);
-        }
-
-        if use_keyfile {
+        tracing::debug!(?dry_run, "Running systemd-repart");
+        let arg_keyfile = use_keyfile.then(|| {
             let keyfile_path = consts::LUKS_KEYFILE_PATH;
             tracing::debug!("Using keyfile for systemd-repart: {keyfile_path}");
-            args.push("--key-file");
-            args.push(keyfile_path);
-        }
-
-        args.extend(&[blockdev.to_str().unwrap()]);
-
-        tracing::debug!(?dry_run, ?args, "Running systemd-repart");
+            ["--key-file", keyfile_path]
+        });
 
         let repart_cmd = Command::new("systemd-repart")
-            .args(args)
+            .args(["--dry-run", if dry_run { "yes" } else { "no" }])
+            .args(["--definitions", cfgdir.to_str().unwrap()])
+            .args(["--empty", "force", "--offline", "false", "--json", "pretty"])
+            .args(["--copy-source", &copy_source].iter().filter(|_| is_bootc))
+            .args(arg_keyfile.iter().flatten())
             .stdout(Stdio::piped())
             .stderr(Stdio::inherit())
             .output()
-            .map_err(|e| eyre!("systemd-repart failed").wrap_err(e))?;
+            .context("systemd-repart failed")?;
 
         if !repart_cmd.status.success() {
             bail!(
@@ -885,10 +851,7 @@ impl FinalInstallationState {
         // Dump systemd-repart output to a file if in debug mode
         if cfg!(debug_assertions) {
             let repart_out_path = std::env::temp_dir().join("readymade-repart-output.json");
-            tracing::debug!(
-                "Dumping systemd-repart output to {}",
-                repart_out_path.display()
-            );
+            tracing::debug!("Dumping systemd-repart output to {repart_out_path:?}");
             std::fs::write(repart_out_path, out)?;
         }
 
@@ -911,9 +874,7 @@ impl InstallationType {
 
     fn set_cgpt_flags(blockdev: &Path) -> Result<()> {
         tracing::debug!("Setting cgpt flags");
-        let blockdev_str = blockdev
-            .to_str()
-            .ok_or_else(|| eyre!("Invalid block device path"))?;
+        let blockdev_str = blockdev.to_str().context("Invalid block device path")?;
         let args = [
             ["add", "-i", "2", "-t"],
             ["kernel", "-P", "15", "-T"],

src/backend/repart_output.rs
@@ -307,12 +307,11 @@ impl RepartOutput {
 }
 
 pub fn is_luks(node: &str) -> bool {
-    let cmd = std::process::Command::new("cryptsetup")
-        .arg("isLuks")
-        .arg(node)
-        .output()
-        .unwrap();
-    cmd.status.success()
+    std::process::Command::new("cryptsetup")
+        .args(["isLuks", node])
+        .status()
+        .expect("cannot run cryptsetup")
+        .success()
 }
 
 // global cache, so we can clean up these devices later
@@ -424,7 +423,7 @@ impl RepartPartition {
 
         let config: RepartConfig = serde_systemd_unit::from_str(&file_config)?;
 
-        tracing::trace!("{:#?}", config);
+        tracing::trace!("{config:#?}");
 
         // get fs type
         let fs_fmt = &config.partition.format;
@@ -444,7 +443,7 @@ impl RepartPartition {
         // if we still have no mount options, use the fallback
 
         if mount_opts.is_empty() {
-            mount_opts.push_str(FALLBACK_OPTS);
+            FALLBACK_OPTS.clone_into(&mut mount_opts);
         }
 
         // let's get the UUID
@@ -506,22 +505,13 @@ impl RepartPartition {
 
         let dump = FALLBACK_DUMP; //todo: is there a config option for this?
 
-        let pass = {
-            // We will be checking from filesystem type
-
-            // or the root device it should be 1. For other partitions it should be 2, or 0 to disable checking.
-
-            // If the root file system is btrfs or XFS, the fsck order should be set to 0 instead of 1.
-
-            if let Some(FileSystem::Btrfs) = fs_fmt {
-                0
-            } else if let Some(FileSystem::Xfs) = fs_fmt {
-                0
-            } else if mntpoint == "/" {
-                1
-            } else {
-                FALLBACK_PASS
-            }
+        // We will be checking from filesystem type
+        // or the root device it should be 1. For other partitions it should be 2, or 0 to disable checking.
+        // If the root file system is btrfs or XFS, the fsck order should be set to 0 instead of 1.
+        let pass = match fs_fmt {
+            Some(FileSystem::Btrfs | FileSystem::Xfs) => 0,
+            _ if mntpoint == "/" => 1,
+            _ => FALLBACK_PASS,
         };
 
         // now let's write the fstab entry
@@ -542,18 +532,14 @@ impl RepartPartition {
 
         tracing::trace!(?config, "Reading mountpoint from config file");
 
-        let mut it = config
-            .partition
-            .mount_point_as_tuple()
-            .into_iter()
-            .peekable();
-        if it.peek().is_some() {
-            Ok(it.collect())
+        let mps = config.partition.mount_point_as_tuple();
+        Ok(if !mps.is_empty() {
+            mps
         } else if let Some(mntpoint) = self.ddi_mountpoint() {
-            Ok(vec![(mntpoint.to_owned(), None)])
+            vec![(mntpoint.to_owned(), None)]
         } else {
-            Ok(vec![])
-        }
+            vec![]
+        })
     }
 }
 

commit dc6b76d5ffeb552c2b8234b053b5a439628b661e

fix(systemd_repart): feed in the blockdev
regression from 62d0a2e8ba661686a90eaf3fe827aa712141ce85

src/backend/install.rs
@@ -864,6 +864,7 @@ impl FinalInstallationState {
             .args(["--empty", "force", "--offline", "false", "--json", "pretty"])
             .args(["--copy-source", &copy_source].iter().filter(|_| is_bootc))
             .args(arg_keyfile.iter().flatten())
+            .arg(blockdev)
             .stdout(Stdio::piped())
             .stderr(Stdio::inherit())
             .output()