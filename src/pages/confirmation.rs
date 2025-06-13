68ace3fdbadab8af29e6efcc84c71d2244019030
madonuko <madonuko@outlook.com>
2025-06-14 03:16:36 +0800

    feat(pages/confirmation): warning dialogues

--- src/pages/confirmation.rs
+++ src/pages/confirmation.rs
@@ -1,27 +1,54 @@
-use std::{os::unix::fs::FileTypeExt, path::PathBuf, time::Duration};
+use std::{os::unix::fs::FileTypeExt, path::PathBuf, process::Command, time::Duration};
 
 use crate::prelude::*;
 
 page!(Confirmation {
     problem: Option<Problem>,
+    root: libhelium::ViewMono,
+    warns: Option<Vec<Warning>>,
 }:
     init(root, sender, model, widgets) {
         gtk::glib::timeout_add(Duration::from_secs(1), move || {
             sender.input(Self::Input::Check);
             gtk::glib::ControlFlow::Continue
         });
-}
-    update(self, message, sender) {
-        StartInstallation => {
-            sender
-                .output(Self::Output::StartInstallation)
-                .unwrap();
+        model.root = root;
+    }
 
-            sender
-                .output(Self::Output::Navigate(NavigationAction::GoTo(
-                    Page::Installation,
-                )))
-                .unwrap();
+    update(self, message, sender) {
+        WarnCancel => {
+            self.warns = None;
+        },
+        WarnConfirm => {
+            let Some(warn) = self.warns.as_mut().expect("no warns??").pop() else {
+                sender
+                    .output(Self::Output::StartInstallation)
+                    .unwrap();
+
+                sender
+                    .output(Self::Output::Navigate(NavigationAction::GoTo(
+                        Page::Installation,
+                    )))
+                    .unwrap();
+                return;
+            };
+            warn.pop(self.root.clone(), &sender);
+        },
+        StartInstallation => {
+            self.warns = Some(Warning::list().collect_vec());
+            let Some(warn) = self.warns.as_mut().expect("no warns??").pop() else {
+                sender
+                    .output(Self::Output::StartInstallation)
+                    .unwrap();
+
+                sender
+                    .output(Self::Output::Navigate(NavigationAction::GoTo(
+                        Page::Installation,
+                    )))
+                    .unwrap();
+                return;
+            };
+            warn.pop(self.root.clone(), &sender);
         },
         Check => {
             self.problem = Problem::detect();
@@ -152,6 +179,9 @@ impl Problem {
         {
             return Some(Self::DeviceMounted(dev.device, dev.mountpoint));
         }
+        if let Ok(procs) = find_open_block_devices(disk.as_ref()) {
+            return Some(Self::DevBlkOpen(disk.to_string(), procs));
+        }
         None
     }
     fn msg(&self) -> String {
@@ -185,4 +215,101 @@ fn find_open_block_devices(device: &str) -> std::io::Result<Vec<usize>> {
                 .filter_map(move |metadata| metadata.file_type().is_block_device().then_some(pid))
         })
         .collect())
+}
+
+#[derive(Debug, Default)]
+enum Warning {
+    #[default]
+    EfiPartFound, // #110
+}
+
+impl Warning {
+    fn efi_part_found() -> Option<Self> {
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!(
+                "lsblk {} -o parttype | grep 'c12a7328-f81f-11d2-ba4b-00a0c93ec93b'",
+                INSTALLATION_STATE
+                    .read()
+                    .destination_disk
+                    .as_ref()
+                    .unwrap()
+                    .devpath
+                    .display()
+            ))
+            .status()
+            .expect("cannot execute sh")
+            .success()
+            .then_some(Self::EfiPartFound)
+    }
+
+    fn list() -> impl Iterator<Item = Self> {
+        [Self::efi_part_found()].into_iter().flatten()
+    }
+
+    fn title(&self) -> String {
+        todo!()
+    }
+
+    fn desc(&self) -> String {
+        todo!()
+    }
+
+    fn pop(
+        self,
+        widget: impl IsA<gtk::Widget>,
+        sender: &ComponentSender<ConfirmationPage>,
+    ) -> Controller<Self> {
+        let root_window = widget.toplevel_window();
+        let mut ctrl = Self::builder()
+            .launch(self)
+            .forward(sender.input_sender(), |b| {
+                if b {
+                    ConfirmationPageMsg::WarnConfirm
+                } else {
+                    ConfirmationPageMsg::WarnCancel
+                }
+            });
+        // XXX: by design yes this is a memleak but we have no choice
+        ctrl.detach_runtime();
+        ctrl.widget().set_transient_for(root_window.as_ref());
+
+        libhelium::prelude::WindowExt::set_parent(
+            ctrl.widget(),
+            Some(&root_window.expect("no root window")),
+        );
+        ctrl.widget().present();
+        ctrl
+    }
+}
+
+#[relm4::component]
+impl relm4::SimpleComponent for Warning {
+    type Init = Self;
+    type Input = ();
+    type Output = bool;
+
+    view! {
+        libhelium::Dialog {
+            set_title: Some(&model.title()),
+            set_icon_name: Some("dialog-error-symbolic"),
+            connect_destroy[sender] => move |_| sender.output(false).unwrap(),
+        },
+    }
+
+    fn init(
+        init: Self::Init,
+        #[expect(unused_assignments)] mut root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = init;
+        let btn = libhelium::Button::new(None, Some(&t!("dialog-installtype-confirm")));
+        root = libhelium::Dialog::builder()
+            .info(model.desc())
+            .primary_button(&btn)
+            .build();
+        let widgets = view_output!();
+        btn.connect_clicked(move |_| sender.output(true).unwrap());
+        ComponentParts { model, widgets }
+    }
 }
